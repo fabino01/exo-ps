c4d739178f5b2b5d4f028e45b312580a8f07f9e2
fabino <fabino@paolo.com>
2018-04-16 16:20:43 +0200

    first commit

---
+++ Script.ps1
@@ -0,0 +1,52 @@
+﻿
+
+$variable = "Powershell"
+
+$variable
+
+$var1 = 10
+$var2 = 10
+$var3 = 10 
+
+<#
+if ($var1 -eq $var2 -and $var2 -eq $var3) {
+    Write-Host "cha marche"
+} else {
+     Write-Host "cha marche pas"
+}
+#>
+
+
+$var1 ="cha va"
+$fruits = @("pomme", "poire", 45, $var1)
+$user = @{Nom="Paolo"; Prenom="Fabino"; Age=32; Role="Beatmaker"}
+<#
+foreach($item in $fruits){
+    $item
+    Write-Host "je tourne"
+}
+#>
+
+<#
+$i = 0;
+
+while ($i  -lt 11){
+    $i * 5
+    $i++;
+}
+#>
+
+function hello($name)
+{
+     "hello " + [string]$name 
+}
+
+hello("fabino")
+hello("Paolo")
+
+World
+
+function world 
+{
+    hello("world")
+}
